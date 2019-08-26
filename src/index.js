64d2c1d504b83213c71786f186205e2e9ca56825
wenzhe <wenzhe@conflux-chain.org>
2019-08-26 15:28:01 +0800

    intl polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/browser';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import intl from 'intl';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -10,6 +11,9 @@ import { updateDispatch } from './utils';
 
 Sentry.init({ dsn: 'https://14d772ae785b46d2979814725a251882@sentry.conflux-chain.org/3' });
 
+if (!global.Intl) {
+  global.Intl = intl;
+}
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
