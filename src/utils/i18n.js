3c1e5b274375119fd2e23606df374e466e90cb8f
wenzhe <wenzhe@conflux-chain.org>
2019-09-30 10:54:32 +0800

    update react i18n

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,81 +1,60 @@
-import React, { PureComponent } from 'react';
-import { injectIntl } from 'react-intl';
+import React, { Suspense, lazy } from 'react';
 import template from 'lodash/template';
+import { createIntl, createIntlCache } from 'react-intl';
+import zhTranslationMessages from '../lang/zh';
+import enTranslationMessages from '../lang/en';
 
-/* eslint react/prop-types: 0 */
-/* eslint react/no-multi-comp: 0 */
-class I18nComp extends PureComponent {
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      fmtStr: '',
-    };
-    const fmtMessageStr = this.updateFmtMsg();
-    if (typeof fmtMessageStr === 'string') {
-      this.state.fmtStr = fmtMessageStr;
-    }
-  }
+const cache = createIntlCache();
+const messageTotal = {
+  en: enTranslationMessages,
+  'zh-CN': zhTranslationMessages,
+};
 
-  componentDidUpdate() {
-    this.updateFmtMsg();
-  }
+let store;
+export const updateStore = s => {
+  store = s;
+};
 
-  updateFmtMsg = () => {
-    const { id, intl } = this.props;
-    const fmtMessageStr = intl.formatMessage({
-      id,
-    });
-    if (fmtMessageStr && fmtMessageStr.then) {
-      fmtMessageStr.then(content => {
-        this.setState({
-          fmtStr: content.default,
-        });
-      });
-    } else {
-      this.setState({
-        fmtStr: fmtMessageStr,
-      });
-    }
-    return fmtMessageStr;
-  };
-
-  render() {
-    const { fmtStr } = this.state;
-    return <span dangerouslySetInnerHTML={{ __html: fmtStr }}></span>;
-  }
-}
-const I18nComp1 = injectIntl(I18nComp);
-
-export function i18n(id) {
-  return <I18nComp1 id={id} />;
-}
-
-let intlTemp;
-class I18nTextComp extends PureComponent {
-  constructor(...args) {
-    super(...args);
-    this.saveIntl = () => {
-      const { intl } = this.props;
-      intlTemp = intl;
-    };
-    this.saveIntl();
-  }
-
-  render() {
-    this.saveIntl();
-    return null;
-  }
-}
-export const I18nText = injectIntl(I18nTextComp);
-export function i18nTxt(id, param) {
-  if (id === null || typeof id === 'undefined' || id === '') {
+export const i18nTxt = (str, param, lang) => {
+  if (str === null || typeof str === 'undefined' || str === '') {
     return '';
   }
-  const txt = intlTemp.formatMessage({
-    id,
+  const language = lang || store.getState().head.user.language;
+  const intl = createIntl(
+    {
+      locale: language,
+      messages: messageTotal[language],
+    },
+    cache
+  );
+  const txt = intl.formatMessage({
+    id: str,
   });
+
   if (param) {
     return template(txt)(param);
   }
   return txt;
-}
+};
+
+export const i18nTxtAsync = (str, lang) => {
+  if (str === null || typeof str === 'undefined' || str === '') {
+    return '';
+  }
+  const language = lang || store.getState().head.user.language;
+  const LazyComp = lazy(() => {
+    return messageTotal[language][str].then(content => {
+      return {
+        default: () => {
+          return <div dangerouslySetInnerHTML={{ __html: content.default }}></div>;
+        },
+      };
+    });
+  });
+
+  return (
+    <Suspense fallback={<div />}>
+      <LazyComp />
+    </Suspense>
+  );
+};
