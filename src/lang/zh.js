7837c42a76ff2269052495816ad5cfa0a26349dd
21paradox <1036339815@qq.com>
2019-11-08 14:42:33 +0800

    add withdraw check

--- src/lang/zh.js
+++ src/lang/zh.js
@@ -444,4 +444,6 @@ export default {
   'checkin.Unavailable': '签到人数已满',
   'Link Copied': '链接已复制',
   'Please select a product.': '请先选择至少一个商品.',
+  'You have insufficient balance in your account': '您的账户没有足够余额',
+  'Minimum withdraw amount 50 FC': '最小提现金额 50 FC',
 };
